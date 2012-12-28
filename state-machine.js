a16420a7fd3c6c965d1b4f76dccb12ecdb55403b
amarr <marron.austin@gmail.com>
2012-12-28 00:25:56 -0800

    Add static reference for clean() and prefix/suffix strings to event methods.

--- state-machine.js
+++ state-machine.js
@@ -1,3 +1,26 @@
+/*
+https://github.com/jakesgordon/javascript-state-machine
+
+Copyright (c) 2012 Jake Gordon and contributors
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
 (function (window) {
 
   var StateMachine = {
@@ -24,6 +47,10 @@
     WILDCARD: '*',
     ASYNC: 'async',
 
+    clean : function(name) {
+        return '__' + name.replace(/:/g, '') + '__';
+    },
+
     //---------------------------------------------------------------------------
 
     create: function(cfg, target) {
@@ -40,10 +67,8 @@
         for (var n = 0 ; n < from.length ; n++)
           map[e.name][from[n]] = e.to || from[n]; // allow no-op transition if 'to' is not specified
       };
-      
-      fsm['clean'] = function(name) {
-          return name.replace(/:/g, '');
-      };
+
+      fsm['clean'] = StateMachine.clean;
 
       if (initial) {
         initial.event = initial.event || 'startup';
@@ -172,4 +197,3 @@
   }
 
 }(this));
-
